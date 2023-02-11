ee04663dcf48ddd0c1b794b90f423bf0b36d23ae
tkattkat <48974763+tkattkat@users.noreply.github.com>
2023-02-11 14:58:46 -0800

    Add files via upload

---
+++ fetchAnswer.js
@@ -0,0 +1,34 @@
+
+//var data = 'Jorge is an associate for a large retailer. Sometimes Jorge works alone answering customer questions or moving inventory. Other times, he works with other associates to rearrange displays or complete large orders. Jorge is probably a(n)';
+
+document.querySelector('#search').addEventListener('click', () => {
+    const data = document.querySelector("#queryAnswer").value;
+    var config = {
+        method: 'post',
+      maxBodyLength: Infinity,
+        url: 'https://homework-api.herokuapp.com/',
+        headers: { 
+          'Content-Type': 'text/plain'
+        },
+        data : data
+      };
+
+       //console.log(question)
+      axios(config).then((result) => {
+          console.log(result.data[0].answer)
+          document.querySelector('#questionText').innerText = `Answer: ${result.data[0].answer}`;
+          document.querySelector('#answerText').innerText = `Question: ${result.data[0].question}`;
+      }).catch((err) => {
+          
+      });
+    }
+)
+
+const clearInput = () => {
+    const input = document.getElementsByTagName("input")[0];
+    input.value = "";
+ 
+  }
+  
+  const clearBtn = document.getElementById("clear-btn");
+  clearBtn.addEventListener("click", clearInput);
